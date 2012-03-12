336025967916e0cebbe0f90cd0492bcace9ed188
Rahul Priyadarshi <rahulpriyadarshi@php.net>
2012-03-12 07:43:54 +0000

    Version change for pdo_ibm-1.3.3 release

--- php_pdo_ibm.h
+++ php_pdo_ibm.h
@@ -22,7 +22,7 @@
 #ifndef PHP_PDO_IBM_H
 #define PHP_PDO_IBM_H
 
-#define PDO_IBM_VERSION "1.3.2"
+#define PDO_IBM_VERSION "1.3.3"
 
 extern zend_module_entry pdo_ibm_module_entry;
 #define phpext_pdo_ibm_ptr &pdo_ibm_module_entry
